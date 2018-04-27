17c1e5a3c25d4afee7156b834d5ae94c16d73f48
zengm@office_desktop <iamzmstone@gmail.com>
2018-04-27 14:58:02 +0800

    change hard-code sms user password to config file

--- lib/utils/sms.rb
+++ lib/utils/sms.rb
@@ -2,7 +2,7 @@ require 'tiny_tds'
 
 class SMS
   def self.send_sms(phone_nbr, content)
-    client = TinyTds::Client.new(username: 'userkhzc', password: '298dkKsdk,8d', host: '202.107.225.201', port: 2010, database: 'sxzjcn', tds_version: '7.0')
+    client = TinyTds::Client.new(username: ENV['sms_user'], password: ENV['sms_pwd'], host: '202.107.225.201', port: 2010, database: 'sxzjcn', tds_version: '7.0')
     sql = "Insert into SMSend_khzc_xj(DestTermID, MsgLen, MsgContent) VALUES ('#{phone_nbr}', 0, '#{content}')"
     result = client.execute(sql)
     result.insert()
