904df04079a95adf3a269ce5ef327567a1743499
zengm <iamzmstone@gmail.com>
2018-04-26 23:49:06 -0700

    fix a mistake in sms class

--- lib/utils/sms.rb
+++ lib/utils/sms.rb
@@ -3,7 +3,7 @@ require 'tiny_tds'
 class SMS
   def self.send_sms(phone_nbr, content)
     client = TinyTds::Client.new(username: 'userkhzc', password: '298dkKsdk,8d', host: '202.107.225.201', port: 2010, database: 'sxzjcn', tds_version: '7.0')
-    sql = "INSERT INTO SMSend_SZCG(DestTermID, MsgLen, MsgContent) VALUES ('#{phone_nbr}', 0, '#{content}')"
+    sql = "Insert into SMSend_khzc_xj(DestTermID, MsgLen, MsgContent) VALUES ('#{phone_nbr}', 0, '#{content}')"
     result = client.execute(sql)
     result.insert()
     client.close()
